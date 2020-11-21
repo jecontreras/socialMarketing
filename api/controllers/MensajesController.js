a0316782507d3a33ac66ee8826349bb27f2cc5e7
jecontreras <joseeduar147@gmail.com>
2020-11-20 22:13:25 -0500

    cambios new

--- api/controllers/MensajesController.js
+++ api/controllers/MensajesController.js
@@ -12,10 +12,12 @@ Procedures.querys = async(req, res)=>{
     let params = req.allParams();
     let resultado = Object();
     resultado = await QuerysServices(Mensajes,params);
+    console.log("*** params", params );
     for( let row of resultado.data ){
         if( row.creado ) row.creado  = await User.findOne( { id: row.creado } ).populate('rol');
         if( row.empresa ) row.empresa = await Empresa.findOne( { id: row.empresa });
     }
+    console.log( "********Cantidad mensaje", resultado.data.length );
     return res.ok( { status: 200, ...resultado } );
 }
 
@@ -58,21 +60,22 @@ Procedures.probando = async(req, res)=>{
 Procedures.getPlataformas = async( req, res )=>{
     let params = req.allParams();
     let resultado = Object();
-    console.log("****", params);
+    console.log("**** params getPlatform", params);
     
     resultado = await RequestServices.get(  params.url, 1);
     if(!resultado) return resultado;
     //validando si ya creo el mensajeNumeros
     let validando = await Procedures.validandoMensajes( { id: params.id });
-    // console.log("****", validando);
+    console.log("**** validador", validando);
     if( !validando ) {
         let listPaginado = await MensajesServices.getUrlPlatform( resultado.count, params.url );
-        // console.log( "*****+++++", listPaginado.length );
+        console.log( "*****+++++", listPaginado.length );
         resultado = await Procedures.creandoMensajesNumeros( { id: params.id, cantidadLista: params.cantidadLista || 1 }, listPaginado );
     }
-    resultado = await MensajesNumeros.find( { mensaje: params.id });
+    resultado = await MensajesNumeros.find( { mensaje: params.id }).limit( 1000 );
     let mensajes = await Mensajes.find( { id: params.id });
     mensajes = mensajes[0];
+    console.log("***total", resultado.length );
     return res.status(200).send( { status: 200, data: { mensaje: mensajes, listaMensaje: resultado } });
     /*let numeros = await Procedures.transFormarEmail( listPaginado );
     await Mensajes.update( { id: params.id }, { emails: numeros })
