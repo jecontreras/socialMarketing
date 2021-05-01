3fcdcb3e9a67123b794ed14e0dccc330b388d106
jecontreras <joseeduar147@gmail.com>
2021-05-01 12:39:59 -0500

    cambios nuevos

--- api/controllers/ArchivosController.js
+++ api/controllers/ArchivosController.js
@@ -15,7 +15,7 @@ Procedures.querys = async(req, res)=>{
 
 Procedures.create = async(req, res)=> {
     let params = req.allParams();
-    let resultado = Array();
+    let resultado = Array(); 
     for(let row of params.archivos) resultado.push(await Archivos.create(row).fetch());
     
     return res.ok( { status: 200, data:[ ...resultado ] } );
